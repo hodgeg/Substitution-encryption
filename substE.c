9add57722fef50ead40aaacea12e8e9353a49a1a
George hodge <hodge.george@rocketmail.com>
2019-04-28 14:09:09 +0000

    hkk

--- substE.c
+++ substE.c
@@ -1,27 +1,34 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
-void substituition(char)
+
+char* substitution(char* message, char* cipher[]);
 
 int main() 
 {
 
+char cipher[26] ="QWERTYUIOPASDFGHJKLZXCVBNM";
+char message = "GETTOCHOPPER";
 
-
-
+substitution (message, cipher);
 return 0;
-
-
 }
     
   
-void substituition(char)
-  
-
- 
- 
+char* substitution(char *message, char *cipher[]);
+{
+int i=0;
 
 
-
+while(message[i]!='\0'&& message[i]>64 && message[i]<91)
+{ 
+    message[i]!=cipher[j];
+        
+        message[i]=cipher+65;
+        {
+            j++
+        }
+puts(message);
+}
+}
 
