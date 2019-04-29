27bc6c5fff51a2c8bd5b656c130a2e031e866fb7
George hodge <hodge.george@rocketmail.com>
2019-04-29 00:36:17 +0000

    ALMOST DONE

--- substE.c
+++ substE.c
@@ -1,34 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-
-char* substitution(char* message, char* cipher[]);
+void substitution(char*, char*);
 
 int main() 
 {
-
+    
 char cipher[26] ="QWERTYUIOPASDFGHJKLZXCVBNM";
-char message = "GETTOCHOPPER";
+char message[40] = "ABCDEF";
 
-substitution (message, cipher);
+//char * theanswer;
+substitution(message, cipher);
+//int i=0;
+//while (theanswer!=0)
+//{
+//    printf("%c", theanswer[i]);
+//    i++;
+//}
+
 return 0;
 }
     
-  
-char* substitution(char *message, char *cipher[]);
+ 
+ 
+void substitution(char *message, char *cipher)
 {
 int i=0;
+int j=0;
 
-
-while(message[i]!='\0'&& message[i]>64 && message[i]<91)
-{ 
-    message[i]!=cipher[j];
-        
-        message[i]=cipher+65;
+while(message[i]!= 0)
+{
+    if (message[i]>64 &&message[i]<91)
+    { 
+        while (message[i]!=cipher[j])
         {
-            j++
+                j++;   
         }
+             message[i]=j+65;
+             
+    }
+        i++;
+        j=0;
+}
 puts(message);
 }
-}
-
